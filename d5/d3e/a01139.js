9e31c8b6e7ed4971b8e36e0247ac3eaaa01a71c4
BillWaller <billxwaller@gmail.com>
2026-04-07 13:17:52 -0500

    Documentation Updates

--- d5/d3e/a01139.js
+++
@@ -1,7 +0,0 @@
-var a01139 =
-[
-    [ "col", "d5/d3e/a01139.html#a5d4a771a0f5b8f8542a2c235001ca70a", null ],
-    [ "len", "d5/d3e/a01139.html#a5227944302f4c823078fcc29c8205114", null ],
-    [ "line", "d5/d3e/a01139.html#a8fe1ec2a1ec422e83737ccb864bce0da", null ],
-    [ "str", "d5/d3e/a01139.html#a639b26c7f0903f268998f88470840ba0", null ]
-];
--- d9/d9a/a00998.js
+++
@@ -1,7 +0,0 @@
-var a00998 =
-[
-    [ "popup_form", "d9/d9a/a00998.html#a60c229fa5ec76fec4c9398713f5d94b0", null ],
-    [ "popup_menu", "d9/d9a/a00998.html#ab23bd360d05e43d199cc7eebd837256e", null ],
-    [ "popup_pick", "d9/d9a/a00998.html#ae9bfdce1fecee7783df822358ca938b0", null ],
-    [ "popup_view", "d9/d9a/a00998.html#acf10729052fcc22eed486aeac3066e63", null ]
-];
